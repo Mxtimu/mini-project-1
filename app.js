d7990bc7fe1c62af2f8828bcbe08926009597f56
mxtimu <matimujunior3@gmail.com>
2026-03-27 21:16:01 +0200

    added the last API and everything works well on the index.html file.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-async function userDashboard (){
+const btn = document.getElementById("loadUser");
+const output = document.getElementById("output");
+
+btn.addEventListener ("click",async function userDashboard (){
     try{
+        output.innerHTML = "Loading...";
         const userRes = await fetch('https://randomuser.me/api/');
         if(!userRes.ok){
             throw new Error("Something went wrong...")
@@ -8,6 +12,7 @@ async function userDashboard (){
         const user = userData.results[0];
 
         const userName = user.name.first;
+        const userCountry = user.location.country;
 
         // console.log(userName);
 
@@ -17,11 +22,29 @@ async function userDashboard (){
         }
         const ageData = await ageRes.json();
         const userAge = ageData.age;
-        console.log(userAge);
+        // console.log(userAge);
+
+        const genderRes = await fetch(`https://api.genderize.io/?name=${userName}`);
+        if(!genderRes.ok){
+            throw new Error("Something went wrong when trying to get the gender...");
+
+
+        }
+        const genderData = await genderRes.json();
+        const userGender = genderData.gender;
+
+        // console.log(userGender);
+
+        output.innerHTML = `
+            <p><strong>Name:</strong> ${userName}</p>
+            <p><strong>Country:</strong> ${userCountry}</p>
+            <p><strong>Age:</strong> ${userAge}</p>
+            <p><strong>Gender:</strong> ${userGender}</p>
+        `;
     }catch(error){
         console.error("Error: ", error);
         
     }
 }
-
-userDashboard(); 
+ );
+// userDashboard(); 
