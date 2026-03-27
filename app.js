89b5e626d94052fd026af46c106d232d22c14e0d
mxtimu <matimujunior3@gmail.com>
2026-03-27 20:53:52 +0200

    Task: second API fetch is working well and as expected...

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ async function userDashboard (){
 
         const userName = user.name.first;
 
-        console.log(userName);
+        // console.log(userName);
+
+        const ageRes = await fetch(`https://api.agify.io/?name=${userName}`);
+        if(!ageRes.ok){
+            throw new Error("Something went wrong when trying to get the age...");
+        }
+        const ageData = await ageRes.json();
+        const userAge = ageData.age;
+        console.log(userAge);
     }catch(error){
         console.error("Error: ", error);
         
